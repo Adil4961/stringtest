40c687f45393bf6170b36c170ab302c81f057799
Adil4961 <adilofficial.4961@gmail.com>
2023-02-21 22:19:38 +0530

    string test

---
+++ string.js
@@ -0,0 +1,36 @@
+//1
+// let str = "wecodeacademy";
+// let cap1 = str.charAt(0).toUpperCase();
+// console.log(cap1 + str.slice(1));
+
+//2 for example:
+// now insert the word JavaScript at 14 position so output will be :
+// We are learning JavaScript exercises.
+
+// let str = "we are learning exercises ";
+// let sli = str.slice(0, 15);
+// let endsli = str.slice(15, 24);
+// console.log(sli.concat(" JavaScript", endsli));
+
+//3
+//My name is wecode academy and my friend name is Arun Kumar."
+//find count of 'is' in this word is 2.
+// let str = " My name is wecode academy and my friend is is name is Arun Kumar.";
+// console.log(str.match(/is/g).length);
+
+//6
+// let name = prompt("What is your name?");
+// let birthYear = prompt(" What is your birth year?");
+// let address = prompt(" What is your address ?");
+// let countryCode = prompt("What is your country code?");
+// let mobileNumber = number(prompt("What is your mobile number?"));
+// let age = 2023 - birthYear;
+// console.log(
+//   `My name is ${name}. I was born in ${birthYear} and my current age is ${age} years. My address is ${address} and my mobile number is ${countryCode} ${mobileNumber}.`
+// );z
+
+//  7
+//let str = "Wecode Academy";
+// let sli = str.slice(0, 6);
+// let endsli = str.slice(7);
+// console.log(sli.concat(" Hello ", "Wolrd ", endsli))
